ef4a331becc535f99cbb9a52f88740eb1e71c372
kovactom <kovactom>
2018-10-11 22:14:05 +0200

    basic project structure created

---
+++ app/src/main/java/com/vsb/tamz/goaltracker/OverviewActivity.java
@@ -0,0 +1,13 @@
+package com.vsb.tamz.goaltracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OverviewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
