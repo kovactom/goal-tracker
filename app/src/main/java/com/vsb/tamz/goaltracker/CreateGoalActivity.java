427210ff39b3ebdbe4bbd76a53d28abd8ceb0ec6
kovactom <kovactom>
2018-10-28 13:23:57 +0100

    Added last input fields

--- app/src/main/java/com/vsb/tamz/goaltracker/CreateGoalActivity.java
+++ app/src/main/java/com/vsb/tamz/goaltracker/CreateGoalActivity.java
@@ -8,6 +8,8 @@ import android.widget.Spinner;
 public class CreateGoalActivity extends Activity {
 
     private Spinner categorySpinner;
+    private Spinner repeatSpinner;
+    private Spinner notificationSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +17,24 @@ public class CreateGoalActivity extends Activity {
         setContentView(R.layout.activity_create_goal);
 
         categorySpinner = findViewById(R.id.spinner);
+        repeatSpinner = findViewById(R.id.spinner2);
+        notificationSpinner = findViewById(R.id.spinner3);
 
         init();
     }
 
     private void init() {
-        final ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.cg_sp_category, android.R.layout.simple_spinner_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        categorySpinner.setAdapter(arrayAdapter);
+        final ArrayAdapter<CharSequence> categoryArray = ArrayAdapter.createFromResource(this, R.array.cg_sp_category, android.R.layout.simple_spinner_item);
+        categoryArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryArray);
+
+        final ArrayAdapter<CharSequence> repeatArray = ArrayAdapter.createFromResource(this, R.array.cg_sp_repeat, android.R.layout.simple_spinner_item);
+        repeatArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        repeatSpinner.setAdapter(repeatArray);
+
+        final ArrayAdapter<CharSequence> notificationArray = ArrayAdapter.createFromResource(this, R.array.cg_sp_notification, android.R.layout.simple_spinner_item);
+        notificationArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        notificationSpinner.setAdapter(notificationArray);
     }
 
 }
