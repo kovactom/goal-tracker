9217787cd925dad713cfb6abe2794184f7ed2cbb
kovactom <kovactom>
2018-11-03 14:34:00 +0100

    Added menu drawer and toolbar

--- app/src/main/java/com/vsb/tamz/goaltracker/OverviewActivity.java
+++ app/src/main/java/com/vsb/tamz/goaltracker/OverviewActivity.java
@@ -1,16 +1,30 @@
 package com.vsb.tamz.goaltracker;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
-public class OverviewActivity extends Activity {
+public class OverviewActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        toolbar = findViewById(R.id.overview_toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.menu_overview, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void createNewGoal(View view) {
