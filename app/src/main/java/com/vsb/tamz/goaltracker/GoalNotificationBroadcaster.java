8484c8fdd19e90087acb30e6f3bf14f963bc9a37
kovactom <kovactom>
2018-11-26 17:54:33 +0100

    added test notification and alarm

---
+++ app/src/main/java/com/vsb/tamz/goaltracker/GoalNotificationBroadcaster.java
@@ -0,0 +1,26 @@
+package com.vsb.tamz.goaltracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.Toast;
+
+public class GoalNotificationBroadcaster extends BroadcastReceiver {
+
+    public static final String CHANNEL_ID = "GOAL_TRACKER_CHANNEL";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_timeline_24px)
+                .setContentTitle("Upcoming goal")
+                .setContentText("Your daily goal is waiting for you!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(5444144, notificationBuilder.build());
+
+        Toast.makeText(context, "Scheduled notification", Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/vsb/tamz/goaltracker/OverviewActivity.java
+++ app/src/main/java/com/vsb/tamz/goaltracker/OverviewActivity.java
@@ -1,7 +1,15 @@
 package com.vsb.tamz.goaltracker;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -13,10 +21,12 @@ import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class OverviewActivity extends AppCompatActivity {
 
+    private AlarmManager alarmManager;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ListView cardListView;
@@ -25,6 +35,9 @@ public class OverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+        alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        this.createNotificationChannel();
+        this.scheduleNotifications();
 
         toolbar = findViewById(R.id.overview_toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -35,6 +48,7 @@ public class OverviewActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24px);
 
+
         List<GoalCard> data = new ArrayList<>();
         data.add(new GoalCard("Learn programming", "Every day", "30 minutes", "120 / 240 minutes"));
         data.add(new GoalCard("Learn foreign language", "Every week", "60 minutes", "60 / 480 minutes"));
@@ -43,6 +57,18 @@ public class OverviewActivity extends AppCompatActivity {
 
         GoalCardListViewAdapter adapter = new GoalCardListViewAdapter(getApplicationContext(), R.layout.goal_card_list_view_adapter, data);
         cardListView.setAdapter(adapter);
+    }
+
+    private void scheduleNotifications() {
+        Intent intent = new Intent(getApplicationContext(), GoalNotificationBroadcaster.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 17);
+        calendar.set(Calendar.MINUTE, 52);
+        calendar.set(Calendar.SECOND, 30);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
     }
 
     @Override
@@ -64,5 +90,21 @@ public class OverviewActivity extends AppCompatActivity {
     public void createNewGoal(View view) {
         Intent intent = new Intent(this, CreateGoalActivity.class);
         startActivity(intent);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(GoalNotificationBroadcaster.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
